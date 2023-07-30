666002f97f6aa45807f19fe49d1b431bd3e9ef32
Ladzi <ladziladzani@gmail.com>
2023-07-30 20:16:48 +0200

    managing control values

--- Reactive-Forms/src/app/app.component.ts
+++ Reactive-Forms/src/app/app.component.ts
@@ -10,17 +10,42 @@ export class AppComponent {
   
   // this FormGroup should be a instance of the OVERALL form in the view/app.component.html
   // formControl is an instance of the fields within the form...
-  // it can take default values, too...
   registrationForm = new FormGroup({
-    userName: new FormControl("Ladzi031"),
+    userName: new FormControl(""),
     password: new FormControl(""),
     confirmPassword: new FormControl(""),
     address: new FormGroup({
       city: new FormControl(""),
       state: new FormControl(""),
-      postCode: new FormControl("")
+      postalCode: new FormControl("")
     })
   });
-  
+
+  public loadApiData() {
     
+    /*
+     the setValue Method, here receives an object that matches the structure of the this.formGroup, and controlNames as keys (should be the same!)
+    this.registrationForm.setValue({
+      userName: "Ladzi031",
+      password: "password1",
+      confirmPassword: "password1",
+      address: {
+        city: "some Random city",
+        state: "some random State",
+        postalCode: "123456"
+      }
+    });
+
+    */
+    
+    //however patchValue is less strict... it accepts a partial object
+    this.registrationForm.patchValue({
+      userName: "James031",
+      address:{
+        city: "city",
+        state: "state"
+      }
+    });
+
+  }    
 }
